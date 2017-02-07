8a87a888f126cc7b5223bf1563a9f00c6e80778f
Hitoooo <hitomeng@outlook.com>
2017-02-07 10:34:16 +0800

    重写CallBack,从而拦截response

--- baselib/src/main/java/com/mht/baselib/net/callback/NetCallback.java
+++ baselib/src/main/java/com/mht/baselib/net/callback/NetCallback.java
@@ -2,8 +2,15 @@ package com.mht.baselib.net.callback;
 
 import android.os.Handler;
 
+import com.google.gson.Gson;
+import com.mht.baselib.net.response.GsonResponseHandler;
 import com.mht.baselib.net.response.IResponseHandler;
+import com.mht.baselib.net.response.JsonResponseHandler;
+import com.mht.baselib.net.response.RawResponseHandler;
 import com.mht.baselib.net.util.LogUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -46,7 +53,50 @@ public class NetCallback implements Callback {
     @Override
     public void onResponse(Call call, final Response response) throws IOException {
         if (response.isSuccessful()) {// 服务器正确响应
-            LogUtils.i(response.body().toString());
+            final String msg = response.body().string();
+            if (mResponseHandler instanceof GsonResponseHandler) {
+
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        GsonResponseHandler gsonResponseHandler = (GsonResponseHandler) mResponseHandler;
+                        Gson gson = new Gson();
+                        try {
+                            gsonResponseHandler.onSuccess(response.code(), gson.fromJson(msg, gsonResponseHandler.getType()));
+                        } catch (Exception e) {// 打印错误日志,ResponseHandler走失败
+                            LogUtils.e("onResponse parse json failed. ResponseBody = " + msg, e);
+                            gsonResponseHandler.onFailure(response.code(), "onResponse parse json failed. ResponseBody = " + msg);
+                        }
+                    }
+                });
+            } else if (mResponseHandler instanceof JsonResponseHandler) {// 转换JSONObject,存在失败与成功
+                final JsonResponseHandler jsonResponseHandler = (JsonResponseHandler) mResponseHandler;
+                try {
+                    final JSONObject jsonObject = new JSONObject(msg);
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            jsonResponseHandler.onSuccess(response.code(), jsonObject);
+                        }
+                    });
+                } catch (final JSONException e) {
+                    LogUtils.e("onResponse parse json failed. ResponseBody = " + msg, e);
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            jsonResponseHandler.onFailure(response.code(),e.toString());
+                        }
+                    });
+                }
+            } else if (mResponseHandler instanceof RawResponseHandler) {
+                final RawResponseHandler rawResponseHandler = (RawResponseHandler) mResponseHandler;
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        rawResponseHandler.onSuccess(response.code(),msg);
+                    }
+                });
+            }
         } else { // 服务器已相应,但是未找到相关资源
             LogUtils.e("onFailure", "server response code" + response.code());
             mHandler.post(new Runnable() {
